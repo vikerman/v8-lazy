e327fd645ec5eed46e493e0b39bf3576be0269fa
Vikram Subramanian <viks@google.com>
2019-05-23 11:18:19 -0700

    add a lazy route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EmptyComponent } from './empty/empty.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'lazy', loadChildren: './lazy/lazy.module#LazyModule'},
+  { path: '', component: EmptyComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmptyComponent } from './empty/empty.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmptyComponent
   ],
   imports: [
     BrowserModule,
